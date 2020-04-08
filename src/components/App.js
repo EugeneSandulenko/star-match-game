eb4ba6dac21d37ee1dba4b97290b2328297c5951
Sandulenko, Eugene <Eugene.Sandulenko@asynchrony.com>
2020-04-08 11:24:22 -0400

    Save leadership table into localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -10,11 +10,32 @@ class Player {
 }
 
 const StarMatch = () => {
+  let savedPayers =[] ;
+  try {
+    if(window.localStorage.players != undefined) 
+    {
+      savedPayers = JSON.parse(window.localStorage.players);
+    }
+  }catch (e){ 
+    // eslint-disable-next-line no-console
+    console.log(e);
+  }
+
   const [gameId, setGameId] = useState(1);
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState(savedPayers);
 
   const updateledershipboard = (name, secondsLeft) => {
-    const existingRecords = players.filter((p) => p.name == name);
+    let existingRecords = [];
+
+    try{
+      existingRecords = JSON.parse(window.localStorage.players)
+        .filter((p) => p.name == name);
+    }
+    catch(e){
+      // eslint-disable-next-line no-console
+      console.log(e);
+    }
+
     const newPlayer =
       existingRecords.length > 0
         ? new Player(
@@ -24,14 +45,19 @@ const StarMatch = () => {
         )
         : new Player(name, secondsLeft, 1);
 
-    return players
+    const newPayers = players
       .filter((p) => p.name != name)
       .concat(newPlayer)
       .sort((a, b) => b.score - a.score);
+
+    window.localStorage.setItem('players',JSON.stringify(newPayers));
+    return newPayers;
   };
 
   const resetGame = (name, secondsLeft) => {
-    setPlayers(updateledershipboard(name,secondsLeft));
+    const newPlayers = updateledershipboard(name, secondsLeft);
+    setPlayers(newPlayers);
+    localStorage.setItem('players', JSON.stringify(newPlayers));
     setGameId(gameId + 1);
   };
 
